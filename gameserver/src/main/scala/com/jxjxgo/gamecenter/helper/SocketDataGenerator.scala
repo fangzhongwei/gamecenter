a08df85f6f977bee4583d4fdeec1bb798f7a9bd0
fangzhongwei <zhongwei.fang@56qq.com>
2017-03-02 17:00:26 +0800

    game center

--- gameserver/src/main/scala/com/jxjxgo/gamecenter/helper/SocketDataGenerator.scala
+++ gameserver/src/main/scala/com/jxjxgo/gamecenter/helper/SocketDataGenerator.scala
@@ -1,7 +1,7 @@
 package com.jxjxgo.gamecenter.helper
 
 import com.jxjxgo.common.helper.GZipHelper
-import com.lawsofnature.common.edecrypt.DESUtils
+import com.jxjxgo.common.edecrypt.DESUtils
 
 /**
   * Created by fangzhongwei on 2016/12/14.
